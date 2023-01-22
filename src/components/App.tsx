1109151714c0ebe79b5a932e3c2bbcd277306812
mr-hated <izdebski.marek@gmail.com>
2023-01-22 18:38:23 +0100

    Child components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { User } from '../model/Model';
 import { AuthService } from '../services/AuthService';
+import { Login } from './Login';
 
 interface AppState {
   user: User | undefined
@@ -10,10 +11,13 @@ export class App extends React.Component<{}, AppState> {
 
   private authService: AuthService = new AuthService();
 
-  
+
   render() {
     return (
-      <div>App from class works!</div>
+      <div>
+        App from class works!
+        <Login authService={this.authService}/>
+      </div>
     )
   }
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { AuthService } from "../services/AuthService";
+
+
+interface LoginProps {
+  authService: AuthService
+}
+
+interface LoginState {
+  userName: string,
+  password: string,
+  loginAttempted: boolean,
+  loginSuccessful: boolean
+}
+
+
+export class Login extends React.Component<LoginProps, LoginState> {
+
+  state: LoginState = {
+    userName: '',
+    password: '',
+    loginAttempted: false,
+    loginSuccessful: false
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Please login</h2>
+        <form>
+          <input value={this.state.userName} /><br/>
+          <input value={this.state.password} type='password' /><br/>
+          <input type='submit' value='Login'/>
+        </form>
+      </div>
+    )
+  }
+}
