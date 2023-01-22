a31261a271366adddb097aef33044b4bb25fc321
mr-hated <izdebski.marek@gmail.com>
2023-01-22 18:27:18 +0100

    First class component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { User } from '../model/Model';
+import { AuthService } from '../services/AuthService';
 
-function App() {
-  return (
-    <div>App works</div>
-  );
+interface AppState {
+  user: User | undefined
 }
 
-export default App;
+export class App extends React.Component<{}, AppState> {
+
+  private authService: AuthService = new AuthService();
+
+  
+  render() {
+    return (
+      <div>App from class works!</div>
+    )
+  }
+}
