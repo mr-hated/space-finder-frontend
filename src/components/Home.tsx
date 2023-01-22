5538e96b63b10d8ec0278edaf11b1a362e69fc54
mr-hated <izdebski.marek@gmail.com>
2023-01-22 19:48:09 +0100

    Routing - NavBar

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Homes extends React.Component {
+
+
+  render() {
+    return(
+      <div>
+        Welcome to the home page!
+      </div>
+    )
+  }
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Profile extends React.Component {
+
+
+  render() {
+    return(
+      <div>
+        Welcome to the profile page!
+      </div>
+    )
+  }
+}
