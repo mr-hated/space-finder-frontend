c939161f49fc8b05bf9d7a2da0be0afc8115287e
mr-hated <izdebski.marek@gmail.com>
2023-02-02 08:35:52 +0100

    Creating spaces

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,7 @@
 
 
+const spacesUrl = 'https://gel9olol2f.execute-api.eu-west-1.amazonaws.com/prod/'
+
 export const config = {
   REGION: 'eu-west-1',
   USER_POOL_ID: 'eu-west-1_FVnI6BWHW',
@@ -7,5 +9,9 @@ export const config = {
   IDENTITY_POOL_ID: 'eu-west-1:37c1c92c-b351-4a02-8c25-81889892b707',
   TEST_USER_NAME: 'hated3',
   TEST_USER_PASSWORD: 'askjskfT7sdf&',
-  SPACES_PHOTOS_BUCKET: 'spaces-photos-0648f2aaede3'
+  SPACES_PHOTOS_BUCKET: 'spaces-photos-0648f2aaede3',
+  api: {
+    baseUrl: spacesUrl,
+    spacesUrl: `${spacesUrl}spaces`
+  }
 }
